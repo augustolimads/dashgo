87866a1756d67b5f58f5550df4325cc828c5aa6c
Augusto Lima <augustolimads@gmail.com>
2022-04-24 16:06:38 -0300

    Initial commit

---
+++ generators/plopfile.js
@@ -0,0 +1,36 @@
+module.exports = (plop) => {
+  plop.setGenerator('component', {
+    description: 'Create a component',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your component name?'
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/index.tsx',
+        templateFile: 'templates/Component.tsx.hbs'
+      }
+    ]
+  }),
+    plop.setGenerator('page', {
+      description: 'Create a page',
+      prompts: [
+        {
+          type: 'input',
+          name: 'name',
+          message: 'What is your page name?'
+        }
+      ],
+      actions: [
+        {
+          type: 'add',
+          path: '../src/pages/{{pascalCase name}}.tsx',
+          templateFile: 'templates/Page.tsx.hbs'
+        }
+      ]
+    })
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1>HomeTemplate</h1>
+    </div>
+  )
+}
+
+export default Home
